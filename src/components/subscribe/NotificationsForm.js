e4d5172745fe41e542b52e6399ed6f68eeb21350
jeffazvgithubber <jeff.williams@vgw.co>
2019-05-21 09:49:57 +0800

    debug

--- src/components/subscribe/NotificationsForm.js
+++ src/components/subscribe/NotificationsForm.js
@@ -62,7 +62,7 @@ class NotificationsForm extends PureComponent {
     );
   }
   onSubscriptionBtnClick = event => {
-    consolel.log("clicked");
+    console.log("clicked");
     this.getSubscriptionState().then(state => {
       if (state.isPushEnabled) {
         /* Subscribed, opt them out */
